90d01bed105f9ac65f552d8a82d5eac855ed4ba3
ALEKSEYR554 <a.morugov@mail.ru>
2025-02-28 19:55:18 +0400

    glow kletki hitbox fix

--- скрипты/скрипт glow.gd
+++ скрипты/скрипт glow.gd
@@ -1,18 +1,23 @@
-extends Button
+extends Node2D
 # Called when the node enters the scene tree for the first time.
 var color=Color.GOLD
 
 func _ready():
-	self.size=Vector2(60,60)
-	self.position=Vector2(0,0)
-	self.pivot_offset=Vector2(60,60)
+	#self.size=Vector2(60,60)
+	#self.position=Vector2(0,0)
+	#self.pivot_offset=Vector2(60,60)
 	#position=Vector2(-300,-300)
 	#self.z_index=2
+	#var par=get_parent()
+	#par.draw.connect(_drawii.bind())
 	pass # Replace with function body.
 
 
 func _draw():
 	# Your draw commands here
+	#var p_pos=get_parent().position
+	#p_pos=Vector2(-p_pos.x/2)
+	#print("writing Button")
 	draw_circle(Vector2(0,0),30, color)
 	pass
 
